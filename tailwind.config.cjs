dcbfe06de6c0b76491206ac8aef71bf447cd84e9
Mr. Mendez <mrmendez@amvestor.com>
2022-06-29 11:06:13 -0400

    added DaisyUI

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,21 +2,14 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {
-      colors: {
-        'main-dark': '#1b1f24',
-        'shadow-dark': '#22282f',
-      },
-
-      dropShadow: {
-        'icon': '0 0px 5px rgba(0, 0, 0, 0.25)',
-        '4xl': [
-            '0 35px 35px rgba(0, 0, 0, 0.25)',
-            '0 45px 65px rgba(0, 0, 0, 0.15)'
-        ]
-      }
-    },
+    extend: {},
   },
-  plugins: [],
-  darkMode: 'class',
+  plugins: [
+    require("daisyui")
+  ],
+  daisyui: {
+    theme: ["night", "emerald"],
+    darkTheme: "night",
+    lightTheme: "emerald",
+  },
 }
